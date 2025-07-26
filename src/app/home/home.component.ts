7915ab491f766a781e5a070c4ecd22e626e7fa31
Bence Nagy <bence@veldex.at>
2025-07-26 15:13:19 +0200

    Add blog post list to home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,35 @@
-import { Component } from '@angular/core';
-import {NavbarComponent} from '../navbar/navbar.component';
+import { Component, OnInit } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { TPost } from '../../types/post';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [
-    NavbarComponent
-  ],
+  imports: [NavbarComponent, CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  posts: TPost[] = [
+    {
+      id: 1,
+      title: 'Post 1',
+      content: 'Content 1',
+      author: 'Author 1',
+      createdAt: '2022-01-01',
+      updatedAt: '2022-01-01',
+    },
+    {
+      id: 2,
+      title: 'Post 2',
+      content: 'Content 2',
+      author: 'Author 2',
+      createdAt: '2022-01-02',
+      updatedAt: '2022-01-02',
+    },
+  ];
 
+  ngOnInit() {
+    console.log('HomeComponent ngOnInit');
+  }
 }
---
+++ src/types/post.ts
@@ -0,0 +1,9 @@
+// TODO adjust these to the actual backend schema (this is just a placeholder)
+export type TPost = {
+  id: number;
+  title: string;
+  content: string;
+  author: string;
+  createdAt: string;
+  updatedAt: string;
+};
